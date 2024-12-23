06531a93cf25f9d4fb50e0d6cfec6343977d198b
zaiyin <riyuzakikira@gmail.com>
2024-12-23 17:18:32 +0700

    Update _worker.js

--- _worker.js
+++ _worker.js
@@ -5,7 +5,7 @@ import { connect } from 'cloudflare:sockets';
 // [Windows] Press "Win + R", input cmd and run:  Powershell -NoExit -Command "[guid]::NewGuid()"
 let userID = 'f8e285e5-7810-436a-8eee-a7c396115c55';
 
-const proxyIPs = ['23.162.136.169', 'cdn.xn--b6gac.eu.org', 'cdn-all.xn--b6gac.eu.org', 'edgetunnel.anycast.eu.org','cdn-b100.xn--b6gac.eu.org'];
+const proxyIPs = ['cdn.xn--b6gac.eu.org', 'cdn-all.xn--b6gac.eu.org', 'cdn-b100.xn--b6gac.eu.org'];
 //
 let proxyIP = proxyIPs[Math.floor(Math.random() * proxyIPs.length)];
 
